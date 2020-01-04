bcb4650e9874c7300b1da5bb8893df1518bfddd4
문상혁 <msk2292@naver.com>
2020-01-04 14:30:30 +0900

    classpath 추가

--- src/main/java/m/s/h/authentication/AuthenticationApplication.java
+++ src/main/java/m/s/h/authentication/AuthenticationApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
-@PropertySource("information.properties")
 @SpringBootApplication
+@PropertySource("classpath:information.properties")
 public class AuthenticationApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthenticationApplication.class, args);
